acf70d4f6f9c4d4c60431ab99f10f5067e1292eb
Fernando Sales <lfgsnando@gmail.com>
2024-03-11 13:25:38 -0300

    Implement unit test for urls and requests methods

--- LumxTest/Service/LumxService.swift
+++ LumxTest/Service/LumxService.swift
@@ -12,21 +12,21 @@ class LumxTestService {
     
     static func getUpcomingMovies(request: MovieListUpcomingRequest, completion: @escaping (AFDataResponse<Data?>) -> ()) {
         let service: MobileService = .getUpcomingMovies(request)
-        AF.request("https://httpbin.org/anything", method: .get, parameters: Parameters())
+        AF.request(service.url, method: .get, headers: service.headers)
             .validate()
             .response(completionHandler: completion)
     }
     
     static func getPopularMovies(request: MovieListPopularRequest, completion: @escaping (AFDataResponse<Data?>) -> ()) {
         let service: MobileService = .getPopularMovies(request)
-        AF.request(service.url, method: service.method)
+        AF.request(service.url, method: service.method, headers: service.headers)
             .validate()
             .response(completionHandler: completion)
     }
     
     static func getMovie(request: MovieDetailsRequest, completion: @escaping (AFDataResponse<Data?>) -> ()) {
         let service: MobileService = .getMovie(request)
-        AF.request(service.url, method: service.method)
+        AF.request(service.url, method: service.method, headers: service.headers)
             .validate()
             .response(completionHandler: completion)
     }
--- LumxTestTests/MobileServiceTests.swift
+++ LumxTestTests/MobileServiceTests.swift
@@ -1,5 +1,5 @@
 //
-//  MobileServiceTest.swift
+//  MobileServiceTests.swift
 //  LumxTestTests
 //
 //  Created by Fernando on 11/03/24.
@@ -8,17 +8,41 @@
 import XCTest
 @testable import LumxTest
 
-final class MobileServiceTest: XCTestCase {
+final class MobileServiceTests: XCTestCase {
 
     func testPopularMovieListUrlAndMethod() throws {
-        // give:
-        let service = MobileService.getPopularMovies(MovieListPopularRequest())
+        // given:
+        let service: MobileService
+        
+        // when
+        service = .getPopularMovies(MovieListPopularRequest())
         
         // then:
         XCTAssertEqual(service.url, URL(string: "https://api.themoviedb.org/3/movie/popular")!)
         XCTAssertEqual(service.method, .get)
         
+    }
+    
+    func testUpcomingMovieListUrlAndMethod() throws {
+        // given:
+        let service = MobileService.getUpcomingMovies(MovieListUpcomingRequest())
         
+        // then:
+        XCTAssertEqual(service.url, URL(string: "https://api.themoviedb.org/3/movie/upcoming")!)
+        XCTAssertEqual(service.method, .get)
+        
+    }
+    
+    func testMovieDetailsUrlAndMethod() throws {
+        // given:
+        let service: MobileService
+        
+        // when
+        service = .getMovie(MovieDetailsRequest())
+        
+        // then:
+        XCTAssertEqual(service.url, URL(string: "https://api.themoviedb.org/3/movie/")!)
+        XCTAssertEqual(service.method, .get)
     }
 
 }
