d9aee530fa519ed79591ea0b2b419261b011037b
Fernando Sales <lfgsnando@gmail.com>
2024-03-11 22:59:56 -0300

    Create xib for movie list and implements basic components

--- LumxTest/MovieList/MovieListViewController.swift
+++ LumxTest/MovieList/MovieListViewController.swift
@@ -9,21 +9,42 @@ import UIKit
 
 class MovieListViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.register(MovieCoverViewCell.self, forCellWithReuseIdentifier: MovieCoverViewCell.reuseIdentifier)
 
-        // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didChangeSegmentControl(_ sender: UISegmentedControl) {
+        
     }
-    */
+}
 
+extension MovieListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Clicked")
+    }
+}
+
+extension MovieListViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCoverViewCell.reuseIdentifier, for: indexPath) as? MovieCoverViewCell {
+            cell.setupCover(cover: "https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg", title: "fast X", releaseDate: "10/10/10")
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    
 }
