4415809d1cbcec79d3632957b8b9f30bcdfaccc2
Fernando Sales <lfgsnando@gmail.com>
2024-03-12 21:34:07 -0300

    Implement router and entrypoint for MovieListViewController

--- LumxTest/AppDelegate.swift
+++ LumxTest/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = MovieListViewController()
+        window?.rootViewController = MovieListRouter.makeModule()
         window?.makeKeyAndVisible()
         
         return true
